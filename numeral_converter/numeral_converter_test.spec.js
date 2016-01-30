cdaa189355ed7d977bb55226242d3cd062779ad5
Jason Fleagle <jasonfleagle@hotmail.com>
2016-01-30 22:13:02 +0000

    Added failing test to numeral_converter_test

--- numeral_converter/numeral_converter_test.spec.js
+++ numeral_converter/numeral_converter_test.spec.js
@@ -1,1 +1,19 @@
 //Fail test for converter.js
+
+//Test for toRoman function
+var toRoman = require('/converter.js');
+
+describe("toRoman()", function() {
+  it("converts 1", function() {
+    (toRoman(1)).toEqual('I');
+  });
+}); 
+  
+//Test for toDecimal function
+var toDecimal = require('/converter.js');
+
+describe("toDecimal()", function() {
+  it("converts I", function() {
+    (toDecimal("I")).toEqual(1);
+  });
+});
