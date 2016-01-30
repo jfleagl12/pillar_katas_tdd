66c844dc6d6dcc4e86c5d54ea3b1862d61030a53
Jason Fleagle <jasonfleagle@hotmail.com>
2016-01-30 22:31:39 +0000

    Added both functions

--- numeral_converter/converter.js
+++ numeral_converter/converter.js
@@ -1,8 +1,15 @@
 //Need toRoman function for converting decimal to roman numeral
-
+var toRoman = function(num) {
+    
+    
+}
 
 
 
 
 
 //Need toDecimal function for converting roman numeral to decimal
+var toDecimal = function(num) {
+    
+    
+}
