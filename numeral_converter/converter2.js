231be94bf6e02abc05d3bd4f7ed57a97ef2cbf6e
Jason Fleagle <jasonfleagle@hotmail.com>
2016-01-31 22:42:16 +0000

    The toRoman function works, but toDecimal does not

--- numeral_converter/converter2.js
+++ numeral_converter/converter2.js
@@ -2,10 +2,21 @@ var num = prompt("Please type in any number or roman numeral");
 
 
 //Need toRoman function for converting decimal to roman numeral
-var toRoman = function(num) {
+var converter = function(num) {
 // 2 arrays for roman and decimal values
 var decimalValue = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ];
 var romanNumeral = [ 'M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I' ];
+if(num == valueOf(decimalValue)){
+    var decimal = '';   
+ 
+ for (var i = 0; i < romanNumeral.length; i++) {
+  while (romanNumeral[i] <= num) {
+      decimal += decimalValue[i];
+      num -= romanNumeral[i];
+    }
+  }
+  return alert(decimal);
+} else {
 //Variable to store empty string
 var romanized = '';
     
@@ -19,27 +30,7 @@ var romanized = '';
     }
   }
     return alert(romanized);
+ }  
 };
-toRoman(num);
+converter(num);
 
-
-
-
-//Need toDecimal function for converting roman numeral to decimal
-var toDecimal = function(num) {
-// 2 arrays for roman and decimal values
-var decimalValue = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ];
-var romanNumeral = [ 'M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I' ];
- //Variable to store empty string
-var decimal = '';   
- 
- for (var i = 0; i < romanNumeral.length; i++) {
-  while (romanNumeral[i] <= num) {
-      decimal += decimalValue[i];
-      num -= romanNumeral[i];
-    }
-  }
-  return alert(decimal);
-    
-};
-toDecimal(num);
