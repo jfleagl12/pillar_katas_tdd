db67c9cd6ad63484f98d6a2b21cea700af964cad
Jason Fleagle <jasonfleagle@hotmail.com>
2016-01-31 00:21:08 +0000

    added 2 arrays for each function

--- numeral_converter/converter.js
+++ numeral_converter/converter.js
@@ -1,8 +1,11 @@
 //Need toRoman function for converting decimal to roman numeral
 var toRoman = function(num) {
+// 2 arrays for roman and decimal values
+var decimalValue = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ];
+var romanNumeral = [ 'M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I' ];
     
     
-}
+};
 
 
 
@@ -10,6 +13,10 @@ var toRoman = function(num) {
 
 //Need toDecimal function for converting roman numeral to decimal
 var toDecimal = function(num) {
+// 2 arrays for roman and decimal values
+var decimalValue = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ];
+var romanNumeral = [ 'M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I' ];
     
+ 
     
-}
+};
