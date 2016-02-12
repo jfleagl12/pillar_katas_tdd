66325fbb219a1adda8efa961c2bc853e92101a44
Jason Fleagle <jasonfleagle@hotmail.com>
2016-02-12 20:46:00 +0000

    All the tests are now passing. Went through and cleaned up the code.

--- numeral_converter/numeral_converter_test.spec.js
+++ numeral_converter/numeral_converter_test.spec.js
@@ -9,11 +9,36 @@ describe("toRoman()", function() {
   });
 }); 
   
+  describe("toRoman()", function() {
+  it("converts 4", function() {
+    (toRoman(4)).toEqual('IV');
+  });
+}); 
+
+describe("toRoman()", function() {
+  it("converts 5", function() {
+    (toRoman(5)).toEqual('V');
+  });
+}); 
+
+
 //Test for toDecimal function
 var toDecimal = require('/converter.html');
 
 describe("toDecimal()", function() {
   it("converts I", function() {
     (toDecimal("I")).toEqual(1);
+  });
+});
+
+describe("toDecimal()", function() {
+  it("converts IV", function() {
+    (toDecimal("IV")).toEqual(4);
+  });
+});
+
+describe("toDecimal()", function() {
+  it("converts V", function() {
+    (toDecimal("V")).toEqual(5);
   });
 });
